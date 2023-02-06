a2b4e21f78ef7308a536e029f255676b56730da7
Leandro Heck <leoheck@gmail.com>
2023-02-06 17:39:12 -0300

    Improve usage message and trim whitespace

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ JsonRoot *getEmptyJson(){
     "\"buffers\":[]}";
   json = string(gltfbase);
   JSON::JsonDriver jd;
-  return jd.parse(json);    
+  return jd.parse(json);
 }
 
 
@@ -54,7 +54,7 @@ void loadGltf(Accessor *pAcc, const char *pGltfFile, const char *pBinFile){
   if(CHECK_ASSIGN_OBJECT(jo2, mesh->get(0)) < 0){
     printf("error 1\n");
     return;
-  }  
+  }
   if(CHECK_ASSIGN_ARRAY(mesh, jo2->get("primitives")) < 0){
     printf("base primitives failed to load\n");
     return;
@@ -76,15 +76,15 @@ void loadGltf(Accessor *pAcc, const char *pGltfFile, const char *pBinFile){
     printf("no buffer view\n");
     return;
   }
-  
+
   jo2 = new JsonObject(ja);
   ja->addEntry(jo2);
   jo2->addEntry("buffer", 0);
   jo2->addEntry("byteOffset", 0);
   jo2->addEntry("byteLength", isz);
-  
+
   jo2 = new JsonObject(ja);
-  ja->addEntry(jo2);  
+  ja->addEntry(jo2);
   jo2->addEntry("buffer", 0);
   jo2->addEntry("byteOffset", isz);
   jo2->addEntry("byteLength", vsz);
@@ -102,24 +102,24 @@ void loadGltf(Accessor *pAcc, const char *pGltfFile, const char *pBinFile){
   fd = open(pGltfFile, O_CREAT | O_RDWR, S_IWUSR | S_IRUSR);
   if(fd < 0){
     return;
-  }  
+  }
   json = jr->serialize();
   if(write(fd, json.c_str(), json.length()) != json.length()){
-    printf("failed to write gltf\n");        
+    printf("failed to write gltf\n");
   }
   close(fd);
 
   fd = open(pBinFile, O_CREAT | O_RDWR, S_IWUSR | S_IRUSR);
   if(fd < 0){
     return;
-  }  
+  }
 
   for(c = 0; c < idc.size(); c++){
     ip = idc[c];
     if(write(fd, (unsigned char*)&ip, sizeof(ip))!= sizeof(ip)){
       printf("failed during idc write\n");
       close(fd);
-      return;      	
+      return;
     }
   }
   for(c = 0; c < vtx.size(); c++){
@@ -127,20 +127,20 @@ void loadGltf(Accessor *pAcc, const char *pGltfFile, const char *pBinFile){
     if(write(fd, (unsigned char*)&vp, sizeof(vp))!= sizeof(vp)){
       printf("failed during vtx write\n");
       close(fd);
-      return;      	
+      return;
     }
   }
-  close(fd);  
+  close(fd);
 }
 
 int main(int argc, char **argv){
   LoadStep *ls;
   Accessor *acc;
   if(argc != 4){
-    printf("Usage: stp2gltf <stpfile> <gltf file> <bin file>\n");
+    printf("Usage: stp2gltf <stp_file> <gltf_file> <bin_file>\n");
     return -1;
   }
   ls = new LoadStep(argv[1]);
   acc = ls->getAccessors();
-  loadGltf(acc, argv[2], argv[3]);  
+  loadGltf(acc, argv[2], argv[3]);
 }
